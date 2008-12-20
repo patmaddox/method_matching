9fd26783b8ee8d7330aba4b6d8b67b5584928aa3
Pat Maddox <pat.maddox@gmail.com>
2008-12-20 11:59:14 -0800

    specs for multiple method_matchers, and work with existing method_missing

--- spec/method_matching_spec.rb
+++ spec/method_matching_spec.rb
@@ -8,4 +8,45 @@ describe "MethodMatching" do
       end
     }.new.foo_bar(1, "two").should == [:foo_bar, 1, "two"]
   end
+
+  it "should handle two method matchers" do
+    klass = Class.new do
+      method_matching(/^foo_(.*)$/) do |method_name, *args|
+        [method_name, args].flatten
+      end
+
+      method_matching(/^baz_(.*)$/) { |mn, *args| mn }
+    end
+
+    klass.new.foo_bar(1, "two").should == [:foo_bar, 1, "two"]
+    klass.new.baz_chicken.should == :baz_chicken
+  end
+
+  it "should not screw with an existing method_missing" do
+    klass = Class.new do
+      def method_missing(method_name, *args, &block)
+        return :caught if method_name == :from_mm
+        super
+      end
+
+      method_matching(/^foo_(.*)$/) { |mn, *args| mn }
+    end
+
+    klass.new.from_mm.should == :caught
+    klass.new.foo_bar.should == :foo_bar
+  end
+
+  it "should not be clobbered by a new method_missing" do
+    klass = Class.new do
+      method_matching(/^foo_(.*)$/) { |mn, *args| mn }
+
+      def method_missing(method_name, *args, &block)
+        return :caught if method_name == :from_mm
+        super
+      end
+    end
+
+    klass.new.from_mm.should == :caught
+    klass.new.foo_bar.should == :foo_bar
+  end
 end
